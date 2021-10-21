b44bd4a45bef61b93b0da8813d6913856a4f3b23
이해빈 <leehaebin@localhost>
2021-10-21 13:57:39 +0900

    불필요 dependency 제거

--- hello-spring-jpa/src/main/java/com/example/demo/controller/HelloController.java
+++ hello-spring-jpa/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,24 +1,16 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.dao.MemberDao;
-import com.example.demo.persistence.Member;
-import com.example.demo.service.MemberService;
 
 @RestController
 public class HelloController {
 
-	@Autowired
-	MemberService memberService;
+//	@Autowired
+//	MemberService memberService;
 	
-	@RequestMapping(name="/select")
-	public ResponseEntity<Member> selectAll(){
-		
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+//	@RequestMapping(name="/select")
+//	public ResponseEntity<Member> selectAll(){
+//		
+//		return new ResponseEntity<>(HttpStatus.OK);
+//	}
 }
